a83d8a2d5128ed4d6039cd35637251d1f1ddade4
Oleksandr <avpavlov96@gmail.com>
2015-12-25 14:38:20 +0200

    Initial commit

---
+++ src/ua/com/goit/gojava/kickstarter/Kickstarter.java
@@ -0,0 +1,15 @@
+package ua.com.goit.gojava.kickstarter;
+
+/**
+ * Created by alex on 25.12.15.
+ */
+public class Kickstarter {
+    public static void main(String[] args) {
+        Kickstarter application = new Kickstarter();
+        application.run();
+    }
+
+    private void run() {
+        System.out.println("Everyone is creator!");
+    }
+}
