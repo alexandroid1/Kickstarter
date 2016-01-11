a1df14fa304d6340204f58154ff5e151de353f7b
Oleksandr <avpavlov96@gmail.com>
2016-01-11 11:49:44 +0200

    test coverage for class QuoteGenerator added

--- src/ua/com/goit/gojava/kickstarter/Kickstarter.java
+++ src/ua/com/goit/gojava/kickstarter/Kickstarter.java
@@ -1,6 +1,7 @@
 package ua.com.goit.gojava.kickstarter;
 
 import java.util.Arrays;
+import java.util.Random;
 import java.util.Scanner;
 
 /**
@@ -16,7 +17,7 @@ public class Kickstarter {
     }
 
     public void run() {
-        QuoteGenerator generator = new QuoteGenerator();
+        QuoteGenerator generator = new QuoteGenerator(new Random());
         System.out.println(generator.nextQuote());
 
         while (true) {
